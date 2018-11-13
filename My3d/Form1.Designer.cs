d04c3ae19e39ac3c83391f8b74763561ea33fa47
innovation <1424382153@qq.com>
2018-11-13 15:14:33 +0800

    3d项目

--- My3d/Form1.Designer.cs
+++ My3d/Form1.Designer.cs
@@ -28,12 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openGLControl = new SharpGL.OpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl
+            // 
+            this.openGLControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openGLControl.DrawFPS = false;
+            this.openGLControl.Location = new System.Drawing.Point(0, 0);
+            this.openGLControl.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.openGLControl.Name = "openGLControl";
+            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl.Size = new System.Drawing.Size(549, 375);
+            this.openGLControl.TabIndex = 0;
+            this.openGLControl.OpenGLInitialized += new System.EventHandler(this.openGLControl1_OpenGLInitialized);
+            this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl1_OpenGLDraw);
+            this.openGLControl.Resize += new System.EventHandler(this.openGLControl1_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 375);
+            this.Controls.Add(this.openGLControl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private SharpGL.OpenGLControl openGLControl;
     }
 }
 
--- My3d/Form1.cs
+++ My3d/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpGL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,70 @@ namespace My3d
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void openGLControl1_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+
+            SharpGL.OpenGL gl = this.openGLControl.OpenGL;
+            //清除深度缓存 
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            //重置当前指定的矩阵为单位矩阵,将当前的用户坐标系的原点移到了屏幕中心
+            gl.LoadIdentity();
+
+            //坐标轴变换位置到(-1.5,0,-6)
+            gl.Translate(-1.5f, 0f, -6f);
+
+            gl.Begin(OpenGL.GL_TRIANGLES);
+            {
+                //顶点 
+                gl.Vertex(0.0f, 1.0f, 0.0f);
+                //左端点      
+                gl.Vertex(-1.0f, -1.0f, 0.0f);
+                //右端点       
+                gl.Vertex(1.0f, -1.0f, 0.0f);
+            }
+            gl.End();
+
+            //把当前坐标系右移3个单位，注意此时是相对上面(-1.5,0,-6)点定位 
+            gl.Translate(3f, 0f, 0f);
+
+            gl.Begin(OpenGL.GL_QUADS);
+            {
+                gl.Vertex(-1.0f, 1.0f, 0.0f);
+                gl.Vertex(1.0f, 1.0f, 0.0f);
+                gl.Vertex(1.0f, -1.0f, 0.0f);
+                gl.Vertex(-1.0f, -1.0f, 0.0f);
+            }
+            gl.End();
+            gl.Flush();   //强制刷新
+        }
+
+        private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
+        {
+            OpenGL gl = openGLControl.OpenGL;
+            gl.ClearColor(0, 0, 0, 0);
+        }
+
+        private void openGLControl1_Resize(object sender, EventArgs e)
+        {
+            OpenGL gl = openGLControl.OpenGL;
+
+            //  设置当前矩阵模式,对投影矩阵应用随后的矩阵操作
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+
+            // 重置当前指定的矩阵为单位矩阵,将当前的用户坐标系的原点移到了屏幕中心
+            gl.LoadIdentity();
+
+            // 创建透视投影变换
+            gl.Perspective(30.0f, (double)Width / (double)Height, 5, 100.0);
+
+            // 视点变换
+            gl.LookAt(-5, 5, -5, 0, 0, 0, 0, 1, 0);
+
+            // 设置当前矩阵为模型视图矩阵
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
         }
     }
 }
